7a18768a5a54c8e34d7b33d2c1d5295b4fdec826
ssc <m18879135186@163.com>
2022-04-04 15:44:25 +0800

    解决广播变量为null的bug,添加hbase自动建表的方法

--- gmall-realtime/src/main/java/com/atguigu/realtime/app/dwd/DwdDbApp.java
+++ gmall-realtime/src/main/java/com/atguigu/realtime/app/dwd/DwdDbApp.java
@@ -5,21 +5,27 @@ import com.alibaba.fastjson.JSONValidator;
 import com.atguigu.realtime.app.BaseApp;
 import com.atguigu.realtime.bean.TableProcess;
 import com.atguigu.realtime.util.KafkaUtils;
-import org.apache.flink.api.common.state.BroadcastState;
-import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
+import org.apache.flink.api.common.state.*;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.datastream.BroadcastStream;
+import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
 import org.apache.flink.util.Collector;
 import org.apache.flink.util.OutputTag;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * @author sunsicheng
@@ -32,20 +38,97 @@ public class DwdDbApp extends BaseApp implements Serializable {
     JSONObject jsonObject = new JSONObject();
 
     public static void main(String[] args) {
-        new DwdDbApp().init(20001, 1, "ods_db", "testdwddbapp02", "dwddbapp");
+        new DwdDbApp().init(20001, 1, "ods_db", "testdwddbapp03", "dwddbapp");
     }
 
     @Override
     public void run(StreamExecutionEnvironment env, DataStreamSource<String> ds) {
         //读取配置表
         SingleOutputStreamOperator<TableProcess> processStream = readProcessTable(env);
+
         //读取业务数据，简单清洗
         SingleOutputStreamOperator<JSONObject> etlStream = etl(ds);
+
         //分流。
         SingleOutputStreamOperator<Tuple2<JSONObject, TableProcess>> dynamicSplitStream = dynamicSplit(processStream, etlStream);
+
         //数据写入kafka,dwd层
         dynamicSplitStream.addSink(KafkaUtils.kafkaSinkAuto());
-        dynamicSplitStream.getSideOutput(hbaseTag).print("hbase");
+        //数据写入dim层
+        sink2Hbase(dynamicSplitStream.getSideOutput(hbaseTag));
+
+    }
+
+    private void sink2Hbase(DataStream<Tuple2<JSONObject, TableProcess>> stream) {
+
+
+        String phoenixUrl = "jdbc:phoenix:hadoop162,hadoop163,hadoop164:2181";
+        // 1. 先在hbase中建表
+
+        stream.
+                keyBy(x -> x.f1.getSinkTable())
+                .addSink(new RichSinkFunction<Tuple2<JSONObject, TableProcess>>() {
+
+                    private ValueState<Boolean> flag;
+                    private Connection connection;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        //创建phoenix连接
+                        connection = DriverManager.getConnection(phoenixUrl);
+                        flag = getRuntimeContext().getState(new ValueStateDescriptor<Boolean>("flag", Boolean.class));
+
+                    }
+
+                    @Override
+                    public void invoke(Tuple2<JSONObject, TableProcess> value, Context context) throws Exception {
+                        checkTableIfExists(value);
+                    }
+
+                    /**
+                     * 检查hbase里面是否存在表，如果不存在就创建。
+                     * CREATE TABLE IF NOT EXISTS "table_2" (
+                     *       state CHAR(2) NOT NULL,
+                     *       city VARCHAR NOT NULL,
+                     *       population BIGINT
+                     *       CONSTRAINT my_pk PRIMARY KEY (state, city)
+                     * );
+                     */
+                    private void checkTableIfExists(Tuple2<JSONObject, TableProcess> value) throws IOException, SQLException {
+                        TableProcess tableProcess = value.f1;
+                        if (flag.value() == null) {
+                            StringBuilder createSql = new StringBuilder();
+                            createSql.append("CREATE TABLE IF NOT EXISTS ");
+                            createSql.append(tableProcess.getSinkTable());
+                            createSql.append("(");
+                            for (String column : tableProcess.getSinkColumns().split(",")) {
+                                createSql.append(column).append(" varchar, ");
+                            }
+                            createSql.append("CONSTRAINT my_pk PRIMARY KEY (");
+                            createSql.append(tableProcess.getSinkPk() == null ? "id" : tableProcess.getSinkPk());
+                            createSql.append(")");
+                            createSql.append(")");
+                            createSql.append(tableProcess.getSinkExtend() == null ? "" : tableProcess.getSinkExtend());
+
+
+                            System.out.println(createSql.toString());
+                            PreparedStatement ps = connection.prepareStatement(createSql.toString());
+                            ps.execute();
+                            connection.commit();
+                            ps.close();
+                            //更新状态
+                            flag.update(true);
+                        }
+                    }
+
+                    @Override
+                    public void close() throws Exception {
+                        if (connection != null) {
+                            connection.close();
+                        }
+                    }
+                });
+
 
     }
 
@@ -69,34 +152,40 @@ public class DwdDbApp extends BaseApp implements Serializable {
                                                ReadOnlyContext ctx,
                                                Collector<Tuple2<JSONObject, TableProcess>> out) throws Exception {
 
-
                         //1.获取配置流，根据配置流将数据保存到hbase或kafka
                         ReadOnlyBroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(processMapStateDescriptor);
                         //maxwell 初始化数据，类型为 "type":"bootstrap-start"
-                        TableProcess tableProcess =
-                                broadcastState.get(value.getString("table") + "_" + value.getString("type").replaceAll("bootstrap-", ""));
-                        //2.获取保存类型,为hbase 或者kafka
-                        String sinkType = tableProcess.getSinkType();
+                        String key = value.getString("table") + "_" + value.getString("type").replaceAll("bootstrap-",
+                                "");
+                        if (broadcastState.contains(key)) {
 
 
-                        //3.过滤出指定需要保存的字段
-                        JSONObject data = value.getJSONObject("data");
-                        String[] sinkColumns = tableProcess.getSinkColumns().split(",");
-                        for (int i = 0; i < sinkColumns.length; i++) {
-                            if (jsonObject.containsKey(sinkColumns[i])) {
-                                jsonObject.put(sinkColumns[i], data.get(sinkColumns[i]));
+                            TableProcess tableProcess =
+                                    broadcastState.get(value.getString("table") + "_" + value.getString("type").replaceAll("bootstrap-", ""));
+                            //System.out.println("key="+key+","+tableProcess);
+
+                            //2.获取保存类型,为hbase 或者kafka
+                            String sinkType = tableProcess.getSinkType();
+
+
+                            //3.过滤出指定需要保存的字段
+                            JSONObject data = value.getJSONObject("data");
+                            String[] sinkColumns = tableProcess.getSinkColumns().split(",");
+                            for (int i = 0; i < sinkColumns.length; i++) {
+                                if (jsonObject.containsKey(sinkColumns[i])) {
+                                    jsonObject.put(sinkColumns[i], data.get(sinkColumns[i]));
+                                }
+                            }
+                            if (TableProcess.SINK_TYPE_KAFKA.equalsIgnoreCase(sinkType)) {
+                                //业务数据发往kafka
+                                out.collect(Tuple2.of(data, tableProcess));
+                            } else if (TableProcess.SINK_TYPE_HBASE.equalsIgnoreCase(sinkType)) {
+                                //配置数据发往hbase测输出流
+                                ctx.output(hbaseTag, Tuple2.of(value, tableProcess));
+                            } else {
+
                             }
                         }
-                        if (TableProcess.SINK_TYPE_KAFKA.equalsIgnoreCase(sinkType)) {
-                            //业务数据发往kafka
-                            out.collect(Tuple2.of(data, tableProcess));
-                        } else if (TableProcess.SINK_TYPE_HBASE.equalsIgnoreCase(sinkType)) {
-                            //配置数据发往hbase测输出流
-                            ctx.output(hbaseTag, Tuple2.of(value, tableProcess));
-                        } else {
-
-                        }
-
                         //清空数据，重复使用
                         jsonObject.clear();
                     }
@@ -109,7 +198,9 @@ public class DwdDbApp extends BaseApp implements Serializable {
                         //1.获取广播状态
                         BroadcastState<String, TableProcess> broadcastState = ctx.getBroadcastState(processMapStateDescriptor);
                         //2.将配置数据更新到广播状态,key为source_table和operate_type共同确定
+                        //System.out.println(value.getSourceTable() + "_" + value.getOperateType());
                         broadcastState.put(value.getSourceTable() + "_" + value.getOperateType(), value);
+
                     }
                 });
 
