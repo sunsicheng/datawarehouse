6cddee1b95007c6033386b7e447618ab61cdef91
ssc <m18879135186@163.com>
2022-04-07 00:01:05 +0800

    uv指标计算

---
+++ gmall-realtime/src/main/java/com/atguigu/realtime/app/dwm/UvApp.java
@@ -0,0 +1,93 @@
+package com.atguigu.realtime.app.dwm;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.JSONValidator;
+import com.atguigu.realtime.app.BaseApp;
+import com.google.common.collect.Lists;
+import org.apache.flink.api.common.eventtime.SerializableTimestampAssigner;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * @author sunsicheng
+ * @version 1.0
+ * @date 2022/4/5 20:44
+ */
+public class UvApp extends BaseApp {
+    public static void main(String[] args) {
+        new UvApp().init(30001, 1, "dwd_page_log", "testUV06", "uvapp");
+    }
+
+    @Override
+    public void run(StreamExecutionEnvironment env, DataStreamSource<String> ds) {
+        ds.flatMap(new FlatMapFunction<String, JSONObject>() {
+            @Override
+            public void flatMap(String value, Collector<JSONObject> out) throws Exception {
+                if (JSONValidator.from(value).validate()) {
+                    out.collect(JSONObject.parseObject(value));
+                }
+            }
+        })
+                .assignTimestampsAndWatermarks(WatermarkStrategy
+                        .<JSONObject>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+                        .withTimestampAssigner(new SerializableTimestampAssigner<JSONObject>() {
+                            @Override
+                            public long extractTimestamp(JSONObject element, long recordTimestamp) {
+                                return element.getLong("ts");
+                            }
+                        }))
+                .keyBy(x -> x.getJSONObject("common").getString("mid"))
+                .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+                .process(new ProcessWindowFunction<JSONObject, JSONObject, String, TimeWindow>() {
+                    private SimpleDateFormat simpleDateFormat;
+                    private ValueState<Long> uvstate;
+
+                    @Override
+                    public void open(Configuration parameters) throws Exception {
+                        uvstate = getRuntimeContext().getState(new ValueStateDescriptor<Long>("uvstate", Long.class));
+                        //TODO yyyy-mm-dd将会报错
+                        simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+                    }
+
+                    @Override
+                    public void process(String s,
+                                        Context context,
+                                        Iterable<JSONObject> elements,
+                                        Collector<JSONObject> out) throws Exception {
+
+                        //1.当该mid设备没有数据产生，状态为空
+                        //2.当该mid设备有数据产生， 但是到了计算第二天uv的时候（第二天时间戳需要使用水印watermark）
+                        String nowWM = simpleDateFormat.format(new Date(context.currentWatermark()));
+                        if (uvstate.value() == null
+                                || !nowWM.equals(simpleDateFormat.format(new Date(uvstate.value())))) {
+                            //使用com.google.guava包，将Iterable直接转换成list
+                            ArrayList<JSONObject> list = Lists.newArrayList(elements);
+                            JSONObject min = Collections.min(list, Comparator.comparing(o -> o.getLong("ts")));
+                            out.collect(min);
+                            uvstate.update(min.getLong("ts"));
+
+                        }
+                    }
+                })
+                .print();
+
+
+    }
+}
